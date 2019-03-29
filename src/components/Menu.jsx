535ff3b621eda773b2fa5d99063630eb953910c0
PavelG <vargan.pg@gmail.com>
2019-03-29 18:13:25 -0400

    Created MENU constant

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,15 @@
 import React from "react";
+
+import { MENU } from "../constants/menu";
 import MenuItemContainer from "../containers/MenuItemContainer";
 
+
 const Menu = () => {
-  return (
-    <div className="right-section">
-      <MenuItemContainer name = "Avocado" price = {5} />
-      <MenuItemContainer name = "Banana" price = {2} />
-      <MenuItemContainer name = "Water" price = {0} />
-      <MenuItemContainer name = "Apple Juice" price = {1} />
-      <MenuItemContainer name = "Carrot Juice" price = {2} />
-      <MenuItemContainer name = "Durian" price = {10} />
-      <MenuItemContainer name = "Jack Fruit" price = {4} />
-      <MenuItemContainer name = "Raw Burritos" price = {5} />
-      <MenuItemContainer name = "Zukini Noodle" price = {7} />
-      <MenuItemContainer name = "Smoosy" price = {3} />
-      <MenuItemContainer name = "Cabbage" price = {111} />
-      <MenuItemContainer name = "Sugar Apple" price = {8} />
-    </div>
-  );
+  var menuItems = MENU.map(item => {
+    return <MenuItemContainer name={item.name} price={item.price} />
+  });
+
+  return <div className="right-section">{menuItems}</div>;
 };
 
 export default Menu;
---
+++ src/constants/menu.js
@@ -0,0 +1,55 @@
+
+const MENU = [
+  { 
+    name: "Tomato Salad", 
+    price: 15 
+  },
+  { 
+    name: "Pad Thai", 
+    price: 13 
+  },
+  { 
+    name: "Zucchini Noodles", 
+    price: 14
+  },
+  { 
+    name: "Jack Fruit Salad", 
+    price: 16
+  },
+  { 
+    name: "Carrot Cake", 
+    price: 9 
+  },
+  { 
+    name: "Blueberry Cheesecake", 
+    price: 10
+  },
+  { 
+    name: "Banana Smoothy", 
+    price: 6 
+  },
+  { 
+    name: "Energy Booster Juice", 
+    price: 7
+  },
+  { 
+    name: "Water", 
+    price: 1
+  },
+  { 
+    name: "Apple Juice", 
+    price: 5
+  },
+  { 
+    name: "Carrot Juice", 
+    price: 4 
+  },
+  { 
+    name: "Whole Cabbage", 
+    price: 25
+  },
+]
+
+module.exports = {
+  MENU
+};
